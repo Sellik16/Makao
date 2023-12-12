00a885c531513600b9f9376ce8e68200e2460738
Sellik16 <ku.sell02@gmail.com>
2023-12-12 19:26:58 +0100

    Dodanie Menu.cs i "hellow world" w MainWindow.xaml.cs

---
+++ Makao/Menu.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Makao
+{
+    public class Button
+    {
+        public int Width { get; set; } = 100;
+        public int Height { get; set; } = 50;
+        public string Text { get; set; } = "Text";
+    }
+
+    public class WybierzKarte : Button
+    {
+        public  WybierzKarte()
+        {
+            Text = "Wybierz Kartę";
+        }
+    }
+    public class Dobierz : Button
+    {
+        public Dobierz()
+        {
+            Text = "Dobierz Kartę";
+        }
+    }
+}
