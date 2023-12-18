801668871a2e8f3806722631149a590ea5efd7d4
Sellik16 <ku.sell02@gmail.com>
2023-12-18 17:10:47 +0100

    rozpisanie właściwości kart

--- Makao/Shuffle.cs
+++ Makao/Shuffle.cs
@@ -9,58 +9,109 @@ namespace Makao
 {
     public class Karta
     {
-        string figura = "As";
-        string kolor = "Pik";
-        string? wlasciwosc = null;
+        int idKarty;
+        string? figura;
+        string? kolor;
+        string? wlasciwosc;
 
     }
-    public class Wlasciwosc
+    //public class Wlasciwosc
+    //{
+    //    public Wlasciwosc()
+    //    {
+    //        void DobierzNext(int x)
+    //        {
+
+    //        }
+    //        void DobierzPrev(int x)
+    //        {
+
+    //        }
+    //        void Czekaj(int x)
+    //        {
+
+    //        }
+    //        void ZmianaKolejka(int x)
+    //        {
+
+    //        }
+    //        void ZmianaGracz(int x)
+    //        {
+
+    //        }
+    //        void NaWszystko()
+    //        {
+    //            //if walet => nie działa
+
+    //        }
+    //        void Blokuj()
+    //        {
+
+    //        }
+    //    }
+    //}
+    public class As : Karta
     {
-        public Wlasciwosc()
-        {
-            void DobierzNext(int x)
-            {
+        //zarządaj zmiany koloru dla kolejnego gracza
 
-            }
-            void DobierzPrev(int x)
-            {
+    }
+    public class Walet : Karta
+    {
+        //zacządaj niefunkcujnej karty dla całej kolejki
 
-            }
-            void Czekaj(int x)
-            {
+    }
+    public class Krolowa : Karta
+    {
+        //można położyć na wszystko oprócz Waleta
 
-            }
-            void ZmianaKolejka(int x)
-            {
+    }
+    public class KrolBitny : Karta
+    {
+        //Królowie kier i pik
+        //pik: kolejny gracz dobiera 5 kart
+        //kier: poprzedni gracz dobiera 5 kart
 
-            }
-            void ZmianaGracz(int x)
-            {
-
-            }
-            void NaWszystko()
-            {
-                //if walet => nie działa
-
-            }
-            void Blokuj()
-            {
-
-            }
-        }
+    }
+    public class KrolNiebitny : Karta
+    {
+        //królowie niebitni, blokują działanie innych królów
+    }
+    public class Dwa : Karta
+    {
+        //dobierz 2
+    }
+    public class Trzy : Karta
+    {
+        //dobierz 3
+    }
+    public class Cztery : Karta
+    {
+        //kolejny gracz czeka kolejkę
+    }
+    public class Niefunkcyjne : Karta
+    {
+        //karty które nie mają konkretnej funkcji
+        //5-10
     }
     public class Talia
     {
         public Talia()
         {
-
+            
         }
-    }
-    public class RozpakowanieTalii
-    {
-        public RozpakowanieTalii()
-        {
 
+    }
+    public class TasowanieTalii  
+    {
+        public TasowanieTalii()
+        {
+            Stack<int> stos = new Stack<int>();
+            
+            
+            //foreach (int karta in stos) //jeśli nie zadziała, zmienić int na var
+            //{
+            //    
+            //}
         }
     }
 }
