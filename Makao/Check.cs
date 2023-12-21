863380fd79202d664d71968e1b9aa9cb35391074
Sellik16 <ku.sell02@gmail.com>
2023-12-21 16:30:42 +0100

    sprawdzanie jaka karta została położona

--- Makao/Check.cs
+++ Makao/Check.cs
@@ -6,11 +6,53 @@ using System.Threading.Tasks;
 
 namespace Makao
 {
-    class Check
+    class Akcja
     {
-        public Check() 
-        {
+        public Karta ZReki { get; set; }
+        public Karta ZeStosu { get; set; }
         
+        public Akcja() 
+        {
+            string Kolor = ZReki.PokazFigure();
+            switch (Kolor)
+            {
+                case "As":
+
+                    break;
+                case "Walet":
+
+                    break;
+                case "Królowa":
+
+                    break;
+                case "Król":
+
+                    break;
+                case "Dwa":
+
+                    break;
+                case "Trzy":
+
+                    break;
+                case "Cztery":
+
+                    break;
+                default: break;
+            }
         }
+        public bool CzyMoznaZagrac(Karta x, Karta y)
+        {
+            bool Czy = false;
+            ZReki = x;
+            ZeStosu = y;
+
+            if (ZReki.PokazFigure() == ZeStosu.PokazFigure())
+                Czy = true;
+            if (ZReki.PokazKolor() == ZeStosu.PokazKolor())
+                Czy = true;
+
+            return Czy;
+        }
+        
     }
 }
